f4c273e68a67f837d4281b85891ddb5388429177
Divinewill Eze Okoro <codewithdiv@users.noreply.github.com>
2021-12-08 23:25:13 +0100

    Chore ⚓: Setup mongodb connection with mongoose.

--- server.js
+++ server.js
@@ -1,7 +1,20 @@
+const mongoose = require("mongoose");
 const dotenv = require("dotenv");
 
 dotenv.config(`./.env`);
+const db = process.env.ATLAS_DB.replace(
+  "<PASSWORD>",
+  process.env.ATLAS_DB_PASSWORD
+);
 
+mongoose
+  .connect(db)
+  .then(() => {
+    console.log(`App connected to database`);
+  })
+  .catch((err) => {
+    console.log(err.message);
+  });
 const app = require("./src/app");
 
 const port = process.env.APP_PORT || 3000;
--- src/app.js
+++ src/app.js
@@ -2,4 +2,7 @@ const express = require("express");
 
 const app = express();
 
+// Middleware that performs body Parsering, reading data from the request body and storing it in req.body
+app.use(express.json());
+
 module.exports = app;
