6d8f89bfb463a22fad9c3d1a53054cd6e55e8c56
Divinewill Eze Okoro <codewithdiv@users.noreply.github.com>
2021-12-20 16:06:34 +0100

    Chore ⚙: Setup sequelize and its CLI

--- server.js
+++ server.js
@@ -1,26 +1,14 @@
-const mysql = require("mysql");
 const dotenv = require("dotenv");
+const { sequelize } = require("./models");
 
 dotenv.config(`./.env`);
-
-const connection = mysql.createConnection({
-  host: process.env.DB_HOST,
-  user: process.env.MYSQL_USER,
-  password: process.env.MYSQL_PASSWORD,
-  database: process.env.DATABASE_NAME,
-});
-
-connection.connect((err) => {
-  if (err) {
-    return console.error(err.message);
-  }
-
-  console.log(`Connected to mysql server at ${process.env.DB_HOST}`);
-});
 
 const app = require("./src/app");
 
 const port = process.env.APP_PORT || 3000;
-app.listen(port, () => {
+app.listen(port, async () => {
   console.log(`App is running on port ${port}...`);
+  // This looks at our models folder and creates database tables according to the table we have.
+  await sequelize.authenticate(); //when changes are made in the model file it will automatically update
+  console.log(`Database Connected`);
 });
