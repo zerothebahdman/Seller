b269d2e98070291c0dde64ddc4f0859064df75af
Divinewill Eze Okoro <codewithdiv@users.noreply.github.com>
2021-12-25 18:19:03 +0100

    Feature 🏋: Authenticated users can create AD's.

--- src/controller/AdsController.js
+++ src/controller/AdsController.js
@@ -1,12 +1,63 @@
-const Ads = require("../../models");
+const { Product, Category, SubCategory } = require("../../models");
+const AppError = require("../utils/AppErrorClass");
 
 exports.getAllAd = async (req, res, next) => {
   try {
-    const ad = await Ads.findAll();
-    res.status(200).json({ status: `success`, ad });
+    const ad = await Product.findAll();
+    res.status(200).json({ status: `success`, number_of_ad: ad.length, ad });
   } catch (err) {
-    res.status(err.status || 500).json(err.message);
+    return next(new AppError(err.message, err.status || 500));
   }
 };
 
-exports.createAd = async (req, res, next) => {};
+exports.createAd = async (req, res, next) => {
+  try {
+    const {
+      name,
+      category,
+      subCategory,
+      quantity,
+      sellingPrice,
+      discountPrice,
+      productCondition,
+      color,
+      size,
+      description,
+      specification,
+      adFirstImage,
+      adSecondImage,
+      adThirdImage,
+      adFourthImage,
+    } = req.body;
+
+    const getCategoryId = await Category.findOne({ where: { id: category } });
+    const getSubCategoryId = await SubCategory.findOne({
+      where: { id: subCategory },
+    });
+
+    const { id, uuid } = req.admin;
+    const product = await Product.create({
+      name,
+      slug: name,
+      adminId: id,
+      adminUuid: uuid,
+      categoryId: getCategoryId.id,
+      subcategoryId: getSubCategoryId.id,
+      quantity,
+      color,
+      size,
+      selling_price: sellingPrice,
+      discount_price: discountPrice,
+      product_condition: productCondition,
+      description,
+      specification,
+      ad_first_image: adFirstImage,
+      ad_second_image: adSecondImage,
+      ad_third_image: adThirdImage,
+      ad_fourth_image: adFourthImage,
+    });
+    res.status(200).json({ status: "success", product });
+  } catch (err) {
+    return next(new AppError(err.message, err.status || 500));
+  }
+};
--- src/routes/adsRouter.js
+++ src/routes/adsRouter.js
@@ -1,8 +1,10 @@
 const express = require("express");
 const { getAllAd, createAd } = require("../controller/AdsController");
+const { adminAuth } = require("../middleware/AuthMiddleware");
+const restricTo = require("../middleware/RolesPermissionsMiddleware");
 
 const router = express.Router();
 
-router.route("/").get(getAllAd).post(createAd);
+router.route("/").get(getAllAd).post(adminAuth, restricTo("vendor"), createAd);
 
 module.exports = router;
