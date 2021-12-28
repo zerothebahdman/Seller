2b78a61119e8f4f1e7958595a3d3044bc453852a
Divinewill Eze Okoro <codewithdiv@users.noreply.github.com>
2021-12-28 16:43:01 +0100

    Feature 🏋: Implemented ad's image upload to cloud.

--- src/controller/AdsController.js
+++ src/controller/AdsController.js
@@ -1,13 +1,45 @@
-const { Product, Category, SubCategory } = require("../../models");
+const cloudinary = require("cloudinary").v2;
+const { Product, Category, SubCategory, Admin } = require("../../models");
 const AppError = require("../utils/AppErrorClass");
+
+cloudinary.config({
+  cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
+  api_key: process.env.CLOUDINARY_API_KEY,
+  api_secret: process.env.CLOUDINARY_API_SECRET_KEY,
+});
 
 exports.getAllAd = async (req, res, next) => {
   try {
-    const ad = await Product.findAll();
+    const ad = await Product.findAll({
+      include: [
+        { model: Admin, as: "admin", attributes: ["uuid", "name", "email"] },
+        { model: Category, as: "category", attributes: ["uuid", "name"] },
+        { model: SubCategory, as: "subcategory", attributes: ["uuid", "name"] },
+      ],
+    });
     res.status(200).json({ status: `success`, number_of_ad: ad.length, ad });
   } catch (err) {
     return next(new AppError(err.message, err.status || 500));
   }
+};
+
+let adImage;
+const images = async (image) => {
+  const publicId = image.split(`/`).pop().replace(".", " ").split(" ")[0];
+  adImage = await cloudinary.uploader.upload(
+    image,
+    {
+      public_id: publicId,
+      folder: "testing",
+      overwrite: false,
+      responsive: true,
+      transformation: { quality: "100", crop: "scale" },
+    },
+    (err) => {
+      if (err) console.log(err);
+    }
+  );
+  return adImage;
 };
 
 exports.createAd = async (req, res, next) => {
@@ -30,6 +62,14 @@ exports.createAd = async (req, res, next) => {
       adFourthImage,
     } = req.body;
 
+    const firstImage = images(adFirstImage);
+    const secondImage =
+      adSecondImage !== undefined ? images(adSecondImage) : undefined;
+    const thirdImage =
+      adSecondImage !== undefined ? images(adThirdImage) : undefined;
+    const fourthImage =
+      adSecondImage !== undefined ? images(adFourthImage) : undefined;
+
     const getCategoryId = await Category.findOne({ where: { id: category } });
     const getSubCategoryId = await SubCategory.findOne({
       where: { id: subCategory },
@@ -51,11 +91,15 @@ exports.createAd = async (req, res, next) => {
       product_condition: productCondition,
       description,
       specification,
-      ad_first_image: adFirstImage,
-      ad_second_image: adSecondImage,
-      ad_third_image: adThirdImage,
-      ad_fourth_image: adFourthImage,
+      ad_first_image: (await firstImage).url,
+      ad_second_image:
+        adSecondImage !== undefined ? (await secondImage).url : undefined,
+      ad_third_image:
+        adThirdImage !== undefined ? (await thirdImage).url : undefined,
+      ad_fourth_image:
+        adFourthImage !== undefined ? (await fourthImage).url : undefined,
     });
+
     res.status(200).json({ status: "success", product });
   } catch (err) {
     return next(new AppError(err.message, err.status || 500));
