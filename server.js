83dd158873463dcf134b256f1dbf68db0a8b4fae
Divinewill Eze Okoro <codewithdiv@users.noreply.github.com>
2022-01-01 22:44:40 +0100

    Chore ⚙: Tracked uncaughtException & unhandledRejection.

--- server.js
+++ server.js
@@ -5,8 +5,17 @@ dotenv.config(`./.env`);
 
 const app = require("./src/app");
 
+process.on("uncaughtException", (err) => {
+  console.error({
+    type: `Unhandled Exception💣! Shutdown In Progress...`,
+    name: err.name,
+    message: err.message,
+  });
+  process.exit(1);
+});
+
 const port = process.env.APP_PORT || 3000;
-app.listen(port, async () => {
+const server = app.listen(port, async () => {
   console.log(`App is running on port ${port}...`);
   if (process.env.NODE_ENV === "development")
     console.log(`App is running on development.`);
@@ -15,4 +24,15 @@ app.listen(port, async () => {
   // This looks at our models folder and creates database tables according to the table we have.
   await sequelize.authenticate(); //when changes are made in the model file it will automatically update
   console.log(`Database Connected`);
+});
+
+process.on("unhandledRejection", (err) => {
+  console.error({
+    type: `Unhandled Rejection!!💣 Shutdown In Progress`,
+    name: err.name,
+    message: err.message,
+  });
+  server.close(() => {
+    process.exit(1);
+  });
 });
